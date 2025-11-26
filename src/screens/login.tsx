cf9b768d23f4aea2bfdd04372b5ef2990afb2c92
daniele <daniele.ripardo@aluno.ce.gov.br>
2025-11-26 20:27:50 -0300

    refactor: alteração da estrutura tailwind

---
+++ src/screens/login.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+
+export function LoginScreen() {
+    return (
+        <View className="flex-1 items-center justify-center bg-pink-200">
+          <Text className="text-6xl">Tela de Login</Text>
+
+       </View>
+    )
+}
