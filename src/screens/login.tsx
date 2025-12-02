f575d7f2526b241534fc956932d17b4b87379ace
daniele <danielemarquesripardo@gmail.com>
2025-12-02 20:50:27 -0300

    feature: add config login e senha

--- src/screens/login.tsx
+++ src/screens/login.tsx
@@ -1,18 +1,37 @@
 import { BotaoCustomizado } from '@/components/botaoCustumizado';
 import { InputCustomizado } from '@/components/inputCustomizado';
+import { useState } from 'react';
 import { Text, View } from 'react-native';
 
 export function LoginScreen() {
+  const [login, setLogin] = useState('');
+
+  const [senha, setSenha] = useState('');
+
+  console.log(senha);
+  console.log(login);
   return (
-    <View className="flex-1 items-center justify-center bg-green-500">
-      <View className="w-full p-5 gap-5 items-center">
+    <View className="flex-1 items-center justify-center">
+      <View className="w-full items-center gap-5 p-5">
         <Text className="text-3xl">Nosso app </Text>
-        <InputCustomizado placeholder={'Login'} />
-        <InputCustomizado placeholder="Senha" />
-        <BotaoCustomizado title="Entrar" />
-        <Text className="text-blue-400">
-          Esqueceu a Senha?
-        </Text>
+        <InputCustomizado
+          placeholder={'Login'}
+          onChangeText={(text) => {
+            setLogin(text);
+          }}
+        />
+
+        <InputCustomizado
+          placeholder="Senha"
+          secureTextEntry={true}
+          onChangeText={(text) => {
+            setSenha(text);
+          }}
+        />
+
+        <BotaoCustomizado title="Entrar"
+         onPress={() => console.log("login", login, "senha", senha)} />
+        <Text className="text-blue-400">Esqueceu a Senha?</Text>
       </View>
     </View>
   );
