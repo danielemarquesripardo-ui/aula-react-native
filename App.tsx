1c7f3e3f3e416ce941a581f67d5c430078c6f4a2
daniele <danielemarquesripardo@gmail.com>
2025-12-17 21:11:04 -0300

    feat: add telas navegador

--- App.tsx
+++ App.tsx
@@ -1,4 +1,3 @@
-
 import { StatusBar } from 'expo-status-bar';
 
 import './global.css';
@@ -6,16 +5,26 @@ import './global.css';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { LoginScreen } from '@/screens/login';
 import { ListaScreen } from '@/screens/lista';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { SomarScreen } from '@/screens/Soma';
+import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
+  const Stack = createNativeStackNavigator();
+
   return (
     <>
-      <SafeAreaView  className="flex-1 ">
-        <ListaScreen />
+      <SafeAreaView className="flex-1 ">
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen name="Login" component={LoginScreen} />
+            <Stack.Screen name="soma" component={SomarScreen} />
+            <Stack.Screen name="Listas" component={ListaScreen} />
+          </Stack.Navigator>
+        </NavigationContainer>
       </SafeAreaView>
 
       <StatusBar style="auto" />
     </>
   );
 }
-
